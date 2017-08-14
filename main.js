9e09122ba79f5922506dd1d431f0e1c4c3939b4b
Courtney Carlson <courtney.trochim@gmail.com>
2017-08-14 14:56:04 -0500

    they see me rollin'. they hatin'.

--- main.js
+++ main.js
@@ -12,21 +12,21 @@ let answer2 = moment().format('DDDD');
 console.log('it is ' +
   answer2 + "th day of the year.");
 
-// let answer3 = moment(x);
-// console.log("It is "
-//   answer3 " seconds into the day.");
-//
-//   function(answer4){
-//   let dls = moment().isDST();
-//   if dls === (true) {
-//       console.log("It " answertrue "during Daylight Savings Time.");
-//       answertrue = "is"
-//   }
-//   else {
-//     console.log("It " answerfalse "during Daylight Savings Time.");
-//       answerfalse = "is not"
-//   }
-// }
+let answer3 = moment().unix();
+console.log('It is ' +
+  answer3 + ' seconds into the day.');
+
+  function(answer4){
+  let dls = moment().isDST();
+  if dls === (true) {
+      console.log("It " answertrue "during Daylight Savings Time.");
+      answertrue = "is"
+  }
+  else {
+    console.log("It " answerfalse "during Daylight Savings Time.");
+      answerfalse = "is not"
+  }
+}
 //
 // let answer5 = moment().isLeapYear();
 //
