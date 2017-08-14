9fe91ad6096fb88b085d35d0b8fc910662b79333
Courtney Carlson <courtney.trochim@gmail.com>
2017-08-14 16:14:43 -0500

    Project complete

--- main.js
+++ main.js
@@ -4,24 +4,23 @@ const moment = require('moment')
 const chalk = require('chalk')
 
 let answer1 = moment().format()
-console.log('It is ' + answer1 + ' .');
+console.log('It is ' + chalk.blue(answer1) + '.');
 
 console.log(chalk.blue('hayyyyyyyyyyyyyy'));
 
 let answer2 = moment().format('DDDD');
-console.log('It is ' +
-  answer2 + 'th day of the year.');
+console.log('It is ' + chalk.magenta(answer2) + 'th day of the year.');
 
 let answer3 = moment().unix();
 console.log('It is ' +
-  answer3 + ' seconds into the day.');
+  chalk.cyan(answer3) + ' seconds into the day.');
 
 
   let dls = (moment().isDST() ? ' is' : ' is not')
   // if (dls === true) {
       // console.log('It ' + answertrue + 'during Daylight Savings Time.');
       // answertrue = 'is'
-      console.log('It' + dls + ' during DayLight Savings Time.');
+      console.log('It' + chalk.green(dls) + ' during DayLight Savings Time.');
 
 
   // else {
@@ -39,5 +38,5 @@ let leapYear = moment().isLeapYear();
     console.log('It ' + answer + 'a Leap Year.');
  }
  else {
-   console.log('It is not a Leap Year.');
+   console.log('It' + chalk.red(' is not') + ' a Leap Year.');
  }
