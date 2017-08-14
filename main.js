8aad870e03cea360913aab7d8bb141362b4be24c
Courtney Carlson <courtney.trochim@gmail.com>
2017-08-14 15:39:59 -0500

    Answer 4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("hi shaaaah!!!");
+console.log('hi shaaaah!!!');
 
 const moment = require('moment')
 const chalk = require('chalk')
@@ -10,33 +10,36 @@ console.log(chalk.blue('hayyyyyyyyyyyyyy'));
 
 let answer2 = moment().format('DDDD');
 console.log('it is ' +
-  answer2 + "th day of the year.");
+  answer2 + 'th day of the year.');
 
 let answer3 = moment().unix();
 console.log('It is ' +
   answer3 + ' seconds into the day.');
 
-  function(answer4){
-  let dls = moment().isDST();
-  if dls === (true) {
-      console.log("It " answertrue "during Daylight Savings Time.");
-      answertrue = "is"
-  }
-  else {
-    console.log("It " answerfalse "during Daylight Savings Time.");
-      answerfalse = "is not"
-  }
-}
+
+  let dls = (moment().isDST() ? ' is' : ' is not')
+  // if (dls === true) {
+      // console.log('It ' + answertrue + 'during Daylight Savings Time.');
+      // answertrue = 'is'
+      console.log('It' + dls + ' during DayLight Savings Time.');
+
+
+  // else {
+    // console.log('It ' + answerfalse + 'during Daylight Savings Time.');
+    //   answerfalse = 'is not'
+  // }
+
+// }
 //
 // let answer5 = moment().isLeapYear();
 //
 //  function(answer5){
 //  if leapYear === (true) {
-//      console.log("It " answertrue "during Daylight Savings Time.");
-//      answertrue = "is"
+//      console.log('It ' answertrue 'during Daylight Savings Time.');
+//      answertrue = 'is'
 //  }
 //  else {
-//    console.log("It " answerfalse "during Daylight Savings Time.");
-//      answerfalse = "is not"
+//    console.log('It ' answerfalse 'during Daylight Savings Time.');
+//      answerfalse = 'is not'
 //    }
 //  }
